5ea5e8cce9866545ecf5cdf991259dc27c503917
Yueqi-19 <liyueqi235@gmail.com>
2020-07-16 22:40:52 -0500

    2020-07-16

---
+++ Week_02/49.字母异位词分组/49-字母异位词分组.java
@@ -0,0 +1,24 @@
+class Solution{
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs==null||strs.length==0){
+            return null;
+        }        
+        HashMap<String,List<String>> hash = new HashMap<>();
+        for (int i = 0 ; i < strs.length;i++){
+            char[] s_arr =strs[i].toCharArray();
+            //排序
+            Arrays.sort(s_arr);
+            //映射到key
+            String key = String.valueOf(s_arr);
+            //添加到对应的类中
+            if (hash.containsKey(key)) {
+                hash.get(key).add(strs[i]);
+            } else{
+                List<String> temp = new ArrayList<String>();
+                temp.add(strs[i]);
+                hash.put(key,temp);
+            }
+        }
+        return new ArrayList<List<String>>(hash.values());
+    }
+}
--- Week_02/README.md
+++ Week_02/README.md
@@ -18,3 +18,13 @@ Internally HashMap contains an array of Node and a node is represented as a clas
 
 
 It can be seen that node is containing a reference of its own object. So it’s a linked list.
+
+
+# 两数之和
+
+由于哈希查找的时间复杂度为 O(1)，所以可以利用哈希容器 map 降低时间复杂度
+遍历数组 nums，i 为当前下标，每个值都判断map中是否存在 target-nums[i] 的 key 值
+如果存在则找到了两个值，如果不存在则将当前的 (nums[i],i) 存入 map 中，继续遍历直到找到为止
+如果最终都没有结果则抛出异常
+时间复杂度：O(n)
+
